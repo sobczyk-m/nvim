afca599fcb43530a7554b23837e7882bc6c471c7
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2024-04-06 16:51:43 +0200

    disable running copilot after entering insert mode

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,49 +1,49 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    build = ":Copilot auth",
-    opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>"
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4
-        },
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  -- event = "InsertEnter", -- run Copilot after entering insert mode
+  build = ':Copilot auth',
+  opts = {
+    panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = '[[',
+        jump_next = ']]',
+        accept = '<CR>',
+        refresh = 'gr',
+        open = '<M-CR>',
       },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<D-;>",
-          accept_word = false,
-          accept_line = false,
-          next = "<D-'>",
-          prev = "<D-[>",
-          dismiss = "<D-]>",
-        },
+      layout = {
+        position = 'bottom', -- | top | left | right
+        ratio = 0.4,
       },
-      filetypes = {
-        ["*"] = true, -- allow for all other filetypes and ignore default `filetypes`
-        -- disable filetypes below
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = '<D-;>',
+        accept_word = false,
+        accept_line = false,
+        next = "<D-'>",
+        prev = '<D-[>',
+        dismiss = '<D-]>',
       },
-      copilot_node_command = 'node', -- Node.js version must be > 18.x
-      server_opts_overrides = {},
+    },
+    filetypes = {
+      ['*'] = true, -- allow for all other filetypes and ignore default `filetypes`
+      -- disable filetypes below
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ['.'] = false,
+    },
+    copilot_node_command = 'node', -- Node.js version must be > 18.x
+    server_opts_overrides = {},
   },
 }
