05d429250981012b1e8e285e70b94f3e121d8dd3
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2024-12-15 13:50:37 +0100

    update wtf.nvim

--- lua/custom/plugins/wtf-nvim.lua
+++ lua/custom/plugins/wtf-nvim.lua
@@ -1,25 +1,25 @@
 return {
-  'piersolenski/wtf.nvim',
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-  },
-  opts = {},
-  keys = {
-    {
-      'gW',
-      mode = { 'n', 'x' },
-      function()
-        require('wtf').ai()
-      end,
-      desc = 'Debug diagnostic with AI',
+    'piersolenski/wtf.nvim',
+    dependencies = {
+        'MunifTanjim/nui.nvim',
     },
-    {
-      mode = { 'n' },
-      'gw',
-      function()
-        require('wtf').search()
-      end,
-      desc = 'Search diagnostic with Google',
-    },
-  },
+    opts = {},
+    keys = {
+        {
+            'gW',
+            mode = { 'n', 'x' },
+            function()
+                require('wtf').ai()
+            end,
+            desc = 'Debug diagnostic with AI',
+        },
+        {
+            mode = { 'n' },
+            'gw',
+            function()
+                require('wtf').search()
+            end,
+            desc = 'Search diagnostic with Google',
+        },
+    }
 }
